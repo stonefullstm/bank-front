88af534d01077a6fa2f3ed9382fbdcd358c3845d
stonefullstm <stonefull.stm@gmail.com>
2023-07-18 09:30:22 -0300

    test: Testes de renderização de componentes e interação com estes elementos

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,12 +1,49 @@
 import { describe, expect, it } from "vitest"
-import { render, screen } from "../../test-utils";
+import { render, screen, userEvent, act } from "../../test-utils";
 import App from '../App';
+// import { testData } from "./mocks/testData";
+
+// const { transferencias } = testData;
 
 describe('Testar página BankStatement', () => {
-  it('Testa se elementos são renderizados', () => {
+  it('Elementos deveriam renderizar', () => {
     render(<App />);
     const titulo = screen.getByText(/Bank Statement/i);
     expect(titulo).toBeInTheDocument();
+    const accountInput = screen.getByTestId('bank-account');
+    expect(accountInput).toBeInTheDocument();
+    const initialInput = screen.getByTestId('initial-date');
+    expect(initialInput).toBeInTheDocument();
+    const finalInput = screen.getByTestId('final-date');
+    expect(finalInput).toBeInTheDocument();
+    const operadorInput = screen.getByTestId('operador');
+    expect(operadorInput).toBeInTheDocument();
+    const button = screen.getByRole('button');
+    expect(button).toBeInTheDocument();
   })
-
+  it('Inputs deveriam aceitar dados', async () => {
+    render(<App />);
+    const accountInput = screen.getByTestId('bank-account');
+    expect(accountInput).toBeInTheDocument();
+    const initialInput = screen.getByTestId('initial-date');
+    expect(initialInput).toBeInTheDocument();
+    const finalInput = screen.getByTestId('final-date');
+    expect(finalInput).toBeInTheDocument();
+    const operadorInput = screen.getByTestId('operador');
+    expect(operadorInput).toBeInTheDocument();
+    await act(async () => {
+      await userEvent.clear(accountInput);
+      await userEvent.type(accountInput, '1');
+      await userEvent.clear(initialInput);
+      await userEvent.type(initialInput, '2023-05-07');
+      await userEvent.clear(finalInput);
+      await userEvent.type(finalInput, '2023-07-18')
+      await userEvent.clear(operadorInput);
+      await userEvent.type(operadorInput, 'Sicrano');
+    })
+    expect(accountInput).toHaveValue(1);
+    expect(initialInput).toHaveValue('2023-05-07');
+    expect(finalInput).toHaveValue('2023-07-18');
+    expect(operadorInput).toHaveValue('Sicrano');
+  })
 });
